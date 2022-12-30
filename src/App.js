f39a0353ac2c6351442fae486611efe9bdd40ad2
Alejo Lang <alejolang123@gmail.com>
2022-12-30 11:43:12 -0300

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MarkdownResult from './components/MarkdownResult';
 
 function App() {
   return (
     <div className="App">
-      
+      <header>asdasd</header>
+      <textarea name="inputTextarea" className='inputTextarea' cols="30" rows="10"></textarea>
+      <MarkdownResult />
     </div>
   );
 }
